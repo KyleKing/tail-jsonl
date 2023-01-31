eac2377a16033ea1fc113aa821990f4fc180e82c
Kyle King <KyleKing@users.noreply.github.com>
2023-01-31 07:39:05 -0500

    bump: version 0.0.1 → 1.0.0

--- tail_jsonl/__init__.py
+++ tail_jsonl/__init__.py
@@ -5,7 +5,7 @@ from warnings import filterwarnings
 from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
 from loguru import logger  # noqa: F401
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 __pkg_name__ = 'tail_jsonl'
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
