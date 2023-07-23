226077746910b50d4e8920f5ed711c8dbe410c60
Kyle King <KyleKing@users.noreply.github.com>
2023-07-23 18:44:33 -0400

    bump: version 1.2.4 → 1.2.5

--- tail_jsonl/__init__.py
+++ tail_jsonl/__init__.py
@@ -2,7 +2,7 @@
 
 import corallium  # Conditionally filter beartype warnings
 
-__version__ = '1.2.4'
+__version__ = '1.2.5'
 __pkg_name__ = 'tail_jsonl'
 
 # ====== Above is the recommended code from calcipy_template and may be updated on new releases ======
