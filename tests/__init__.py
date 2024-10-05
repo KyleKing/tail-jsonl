571935fc390a43cba87a331b03dfb472e3527d14
Kyle King <KyleKing@users.noreply.github.com>
2024-10-05 11:22:11 -0400

    ci: report warnings on test typechecking 3.9

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,8 @@
 import sys
 from os import environ, getenv
 
+# Required for consistent test output
 environ['COLUMNS'] = '120'
 
-DEF_MODE = 'ERROR' if sys.version_info >= (3, 9) else 'WARNING'
+DEF_MODE = 'ERROR' if sys.version_info >= (3, 10) else 'WARNING'
 environ['RUNTIME_TYPE_CHECKING_MODE'] = getenv('RUNTIME_TYPE_CHECKING_MODE', DEF_MODE)
