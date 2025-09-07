4795936b414d3abaab1e0f7ef96cf548a5ad2242
Kyle King <KyleKing@users.noreply.github.com>
2025-09-06 20:51:02 -0600

    feat: add defaults for LogTape

--- tail_jsonl/config.py
+++ tail_jsonl/config.py
@@ -21,9 +21,9 @@ class Keys:
 
     timestamp: list[str] = field(default_factory=lambda: ['timestamp', 'time', 'record.time.repr'])
     level: list[str] = field(default_factory=lambda: ['level', 'levelname', 'record.level.name'])
-    message: list[str] = field(default_factory=lambda: ['event', 'message', 'msg', 'record.message'])
+    message: list[str] = field(default_factory=lambda: ['event', 'message', 'msg', 'record.message', '_message'])
 
-    on_own_line: list[str] = field(default_factory=lambda: ['text', 'exception'])
+    on_own_line: list[str] = field(default_factory=lambda: ['text', 'exception', 'error.stack'])
 
     @classmethod
     def from_dict(cls, data: dict) -> Keys:  # type: ignore[type-arg]
